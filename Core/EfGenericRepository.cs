fd251933e548e2dd64d3b7b6160433b96581a10d
Güray Orhan <gurayorhan41@gmail.com>
2020-04-05 21:24:06 +0300

    first

---
+++ Core/EfGenericRepository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using Blog.YazilimciPlatformu.Models;
+using Blog.YazilimciPlatformu.Models.ModelsDB;
+
+namespace Blog.YazilimciPlatformu.Repository
+{
+    public class EfGenericRepository<T> : IGenericRepository<T> where T:class
+    {
+        public readonly ContextDB context;
+
+        public EfGenericRepository(ContextDB x)
+        {
+            this.context = x;
+        }
+
+        public T Getir(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public T Guncelle(T entity)
+        {
+            context.Set<T>().Update(entity);
+            context.SaveChanges();
+            return entity;
+        }
+
+        public T Kaydet(T entity)
+        {
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+            return entity;
+        }
+
+        public List<T> Listele()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public List<T> Listele(Expression<Func<T, bool>> predicate)
+        {
+            return context.Set<T>().Where(predicate).ToList();
+        }
+
+        public bool Sil(int id)
+        {
+            return Sil(Getir(id));
+        }
+
+        public bool Sil(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            return context.SaveChanges() > 0;
+        }
+        public T Getir(Expression<Func<T, bool>> predicate)
+        {
+            return context.Set<T>().Where(predicate).SingleOrDefault();
+        }
+
+        public int KayitSayisi()
+        {
+            return context.Set<T>().Count();
+        }
+
+        public int KayitSayisi(Expression<Func<T, bool>> predicate)
+        {
+            return context.Set<T>().Where(predicate).Count();
+        }
+
+        public IQueryable<TResult> GetAllSelect<TResult>(Expression<Func<T, TResult>> select)
+        {
+            return context.Set<T>().Select(select);
+        }
+
+        public IQueryable<TResult> GetAllSelect<TResult>(Expression<Func<T, bool>> predicate, Expression<Func<T, TResult>> select)
+        {
+            return context.Set<T>().Where(predicate).Select(select);
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(true);
+        }
+    }
+}
---
+++ MVC/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using RezervasyonMvc.MvcUI.Models;
+
+namespace RezervasyonMvc.MvcUI.Repository
+{
+    public interface IGenericRepository<T>:IDisposable where T:class
+    {
+        T Kaydet(T entity);
+        T Getir(int id);
+        T Getir(Expression<Func<T, bool>> predicate);
+        List<T> Listele();
+        List<T> Listele(Expression<Func<T, bool>> predicate);
+        IQueryable<TResult> GetAllSelect<TResult>(Expression<Func<T, TResult>> select);
+        IQueryable<TResult> GetAllSelect<TResult>(Expression<Func<T, bool>> predicate, Expression<Func<T, TResult>> select);
+        bool Sil(int id);
+        bool Sil(T entity);
+        T Guncelle(T entity);
+        int KayitSayisi();
+        int KayitSayisi(Expression<Func<T, bool>> predicate);
+
+    }
+}
